78f4c5ce3499a5e83386e8de8a20a0e4a08d9db8
Kyle Allan <kyle@stitchdata.com>
2020-01-03 21:34:59 +0000

    bump to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-slack',
-      version='0.0.1',
+      version='0.0.2',
       description='Singer.io tap for extracting data from the Slack Web API',
       author='dwallace@envoy.com',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
